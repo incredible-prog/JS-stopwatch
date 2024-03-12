22d9c7452edda62d6bc0900363829927e19210b5
Incredible Programmer <75069098+incredible-prog@users.noreply.github.com>
2024-03-12 13:13:19 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// Code by incredible_prog
+
+const display = document.getElementById("timeContent")
+let timer = null;
+
+let startTime = 0;
+let elaspedTime = 0;
+let isRunning = false;
+
+function start(){
+    if (!isRunning){
+        startTime = Date.now() - elaspedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+
+    }
+}
+
+
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elaspedTime = Date.now() - startTime;
+        isRunning = false; 
+
+    }
+}
+function reset(){
+    clearInterval(timer);
+    startTime = 0;
+    elaspedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00"
+}
+
+function update(){
+    const currentTime = Date.now();
+    elaspedTime = currentTime - startTime;
+    let hours = (Math.floor(elaspedTime / ( 1000 * 60 * 60))).toString().padStart(2, 0);
+    let minutes = (Math.floor(elaspedTime/(1000 * 60) % 60)).toString().padStart(2, 0);
+    let seconds = (Math.floor(elaspedTime/1000 % 60)).toString().padStart(2, 0);
+    let milliseconds = (Math.floor(elaspedTime % 1000 / 10)).toString().padStart(2, 0);
+
+    //hours = String(hours).padstart(2, 0);
+    //minutes = String(minutes).padstart(2, 0);
+    //seconds = String(seconds).padstart(2, 0);
+    //milliseconds = String(milliseconds).padstart(2, 0);
+    display.textContent =  `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+}
